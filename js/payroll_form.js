8741ae0dd070c75a15f04ddca9e882ffa1f910e9
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-06-20 18:52:47 +0530

    Added reset function

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -92,6 +92,31 @@ const getSelectedValues = (propertyValue) => {
     return setItems;
 }
 
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('.salary-output','400000');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2021');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    }); 
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+} 
+
 
 
 
