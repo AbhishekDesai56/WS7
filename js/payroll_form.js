6da83a1c962622d6e72d739ab6d8194d11001020
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-06-20 16:36:55 +0530

    Modified the Javascript code executed when DOM Content completely get loaded

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -1,35 +1,41 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+        if (name.value.length == 0) {
+            textError.textContent = "";
+            return;
+        }
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+            
+        } catch (e) {
+            textError.textContent  = e;
+        }
+    });
+    
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
     output.textContent = salary.value;
-});
+    salary.addEventListener('input', function() {
+        output.textContent = salary.value;
+    });
 
-const names = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-names.addEventListener('input', function() {
-    try {
-        (new EmployeePayrollData()).name = names.value;
-        textError.textContent = "";
-        
-    } catch (e) {
-        textError.textContent  = e;
-    }
-});
-
-const date = document.querySelector('#date');
-const textErrorDate = document.querySelector('.text-error-date');
-date.addEventListener('input', function() {
-    const startDate = new Date(Date.parse(document.querySelector('#day').value +" "+
-                                          document.querySelector('#month').value +" "+ 
-                                          document.querySelector('#year').value))
-    try {
-        (new EmployeePayrollData()).startDate = startDate;
-        textErrorDate.textContent = "";
-        
-    } catch (e) {
-        textErrorDate.textContent  = e;
-    }
+    const date = document.querySelector('#date');
+    const textErrorDate = document.querySelector('.text-error-date');
+    date.addEventListener('input', function() {
+        const startDate = new Date(Date.parse(document.querySelector('#day').value +" "+
+                                            document.querySelector('#month').value +" "+ 
+                                            document.querySelector('#year').value))
+        try {
+            (new EmployeePayrollData()).startDate = startDate;
+            textErrorDate.textContent = "";
+            
+        } catch (e) {
+            textErrorDate.textContent  = e;
+        }
+    });
 });
 
 const save = () => {
@@ -37,5 +43,9 @@ const save = () => {
     employeePayrollData.name = names.value;
     alert(employeePayrollData.toString());
 }
+
+
+
+
 
 
